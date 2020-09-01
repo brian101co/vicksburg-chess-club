096d9aa9bfc9d2401374a84bb1460b9ea7926c28
Brian Oliver <brianoliver@MacBook-Pro-3.local>
2020-09-01 07:22:07 -0500

    added tests for pages app and finished templates for tournament app

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class TestHomePage(TestCase):
+    def test_home_page(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/index.html')
+
+class TestAboutPage(TestCase):
+    def test_home_page(self):
+        response = self.client.get(reverse('about'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'pages/about_us.html')
